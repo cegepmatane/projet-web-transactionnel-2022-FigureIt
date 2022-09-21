4388bd05355e2dcaa9619bcc8a2e6b1c64ac2f9c
BenjaminRodot <78915615+BenjaminRodot@users.noreply.github.com>
2022-09-20 20:27:15 -0400

    Ajout de la page Mission #12

---
+++ src/mission.php
@@ -0,0 +1,22 @@
+<?php
+    $titre = "Accueil";
+    $pageActive = "accueil";
+    include "header.php";
+?>
+<div class="container">
+    <h1 class="text-center">A propos de nous</h1>
+    <div>
+    </div>
+
+    <h1 class="text-center">Nos objectifs</h1>
+    <div>
+    </div>
+
+    <h1 class="text-center">Notre systeme économique</h1>
+    <div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
