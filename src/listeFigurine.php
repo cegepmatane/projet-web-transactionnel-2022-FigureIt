aabf3beddbaf75b01c85794c63ce980cb2242b17
Moa <lucas.heidet@orange.fr>
2022-09-21 19:05:08 +0200

    #10 Début et fin de la page liste figurine

--- src/listeFigurine.php
+++ src/listeFigurine.php
@@ -1,2 +1,442 @@
-<?php ?>
-<div>Hello liste figurine</div>
+<?php
+    $titre = "Liste Figurine";
+    $pageActive = "listeFigurine";
+    include "header.php";
+?>
+<div class="container-fluid px-4">
+    <div class="row row-cols-1 row-cols-lg-5 row-cols-md-3 row-cols-sm-2 liste-figurine-row">
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card border-secondary" id="figurine1">
+                <img src="./pages/Images/Placeholder_view_vector.svg" class="card-img-top figurine-img-border" alt="Placeholder">
+
+                <div class="card-body">
+                    <div class="mb-3" id="titre-figurine">
+                        <h6 class="font-weigth-semibold">
+                            <a href="details.php" class="text-default" style="text-decoration: none !important;">Figurine 1/5 Iron Man</a>
+                        </h6>
+                    </div>
+
+                    <div id="prix">
+                        <h3 class="mb-2 font-weight-semibold">00,00$</h3>
+                    </div>
+
+                    <div class="mb-3 text-muted">Nom du vendeur</div>
+
+                    <div class="d-flex justify-content-end" id="bouton-ajouter-panier">
+                        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
