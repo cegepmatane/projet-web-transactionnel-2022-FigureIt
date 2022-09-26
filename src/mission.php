2d5057121da53df7ba41172c35f4c04e865604b5
Moa <lucas.heidet@orange.fr>
2022-09-26 23:52:22 +0200

    #13 #14 ajout des footers

--- src/mission.php
+++ src/mission.php
@@ -64,7 +64,5 @@
             facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facil
     </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php include "footer.php"?>
 
