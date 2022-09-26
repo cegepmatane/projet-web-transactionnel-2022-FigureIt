7f7bf49963b4479477e42dfb135a5b976ad80bf6
Moa <lucas.heidet@orange.fr>
2022-09-26 23:38:22 +0200

    #14 #16 #18 refactoring du dossier + affichage données bdd

--- src/admin/index.php
+++ src/admin/index.php
@@ -2,21 +2,23 @@
     $titre = "AdminListe";
     $pageActive = "adminListe";
 
-    include "../../accesseur/FigurineDAO.php";
+    include "accesseur/FigurineDAO.php";
     $figurines = FigurineDAO::listerFigurines();
 ?>
 <!DOCTYPE html>
-<html>
+<html class="h-100">
 <head>
     <meta charset="utf-8">
     <title>Admin</title>
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
 
 </head>
-<body>
+<body class="d-flex flex-column h-100">
     <div class="container">
         <div class="header">
             <h1>FigureIt</h1>
@@ -37,10 +39,10 @@
                 <?php foreach ($figurines as $figurine){ 
                     ?>
                     <tr>
-                        <td><?php echo($figurine->vendeur)?></td>
-                        <td><?php echo($figurine->titre)?><td>
-                        <td><?php echo($figurine->quantite)?></td>
-                        <td><?php echo($figurine->prix)?></td>
+                        <td><?= formater($figurine->vendeur)?></td>
+                        <td><?= formater($figurine->titre)?><td>
+                        <td><?= formater($figurine->quantite)?></td>
+                        <td><?= formater($figurine->prix)?></td>
                         <td>
                             <a href="admin_liste.html?id={{ user.id }}">Modifier</a>
                             <a href="admin_liste.html?id={{ user.id }}">Supprimer</a>
@@ -52,9 +54,4 @@
             </table>
             </div>
         </div>
-        <div class="footer">
-            <p>Footer</p>
-        </div>
-    </div>
-</body>
-</html>
+<?php include "../footer.php"; ?>
