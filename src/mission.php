4a7de51b299a1d09e6b50bfb48dd73a201b81898
Moa <lucas.heidet@orange.fr>
2022-09-28 05:58:34 +0200

    #13 text justifié

--- src/mission.php
+++ src/mission.php
@@ -8,14 +8,14 @@
 </head>
 <div class="container">
     <h1 class="text-left">A propos de nous</h1>
-    <div class="description">
+    <div class="description text-justify">
         <p>
             Nous sommes un groupe de 3 développeurs cherchant à partager notre passion pour les figurines d'occasion. 
             Il y a donc Lucas Heidet, Axel Klein et Benjamin Rodot à la tête de ce projet.
     </div>
 
     <h1 class="text-left">Nos objectifs</h1>
-    <div class="description">
+    <div class="description text-justify">
         <p>
             Pour notre part, nous cherchons à élargir le public d'achat des figurines d'occasion. Car dû aux nombreux collectionneurs,
             il est difficile de trouver des figurines d'occasion sans emballages, en bon état et à un prix raisonnable. 
@@ -24,7 +24,7 @@
     </div>
 
     <h1 class="text-left">Notre systeme économique</h1>
-    <div class="description">
+    <div class="description text-justify">
         <p>
             Notre système économique est basé sur 2 points. Le premier sera la publicité, qui permettra de faire connaitre le site,
             et assurer une certaine visibilité. Le second point sera la commission, donc nous ajouterons un pourcentage sur chaque vente
