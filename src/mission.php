15383f920a19d7172cc28643b120ab0b05411c1c
BenjaminRodot <b.a.rodot@orange.fr>
2022-09-27 23:52:14 -0400

    Ajout du contenu de la page Mission #12

--- src/mission.php
+++ src/mission.php
@@ -10,58 +10,25 @@
     <h1 class="text-left">A propos de nous</h1>
     <div class="description">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec aliquam aliquam, nunc nisl
-            tincidunt nunc, eget aliquam nisl nisl sit amet nunc. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facil
+            Nous sommes un groupe de 3 développeurs cherchant à partager notre passion pour les figurines d'occasion. 
+            Il y a donc Lucas Heidet, Axel Klein et Benjamin Rodot à la tête de ce projet.
     </div>
 
     <h1 class="text-left">Nos objectifs</h1>
     <div class="description">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec aliquam aliquam, nunc nisl
-            tincidunt nunc, eget aliquam nisl nisl sit amet nunc. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facil
+            Pour notre part, nous cherchons à élargir le public d'achat des figurines d'occasion. Car dû aux nombreux collectionneurs,
+            il est difficile de trouver des figurines d'occasion sans emballages, en bon état et à un prix raisonnable. 
+            Nous voulons donc créer un site internet qui permettra de trouver les figurines que vous recherchez, avec ou sans emballages,
+            dépendant de votre envie et de votre budget.
     </div>
 
     <h1 class="text-left">Notre systeme économique</h1>
     <div class="description">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec aliquam aliquam, nunc nisl
-            tincidunt nunc, eget aliquam nisl nisl sit amet nunc. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facil
+            Notre système économique est basé sur 2 points. Le premier sera la publicité, qui permettra de faire connaitre le site,
+            et assurer une certaine visibilité. Le second point sera la commission, donc nous ajouterons un pourcentage sur chaque vente
+            pour nous permettre de payer les frais de fonctionnement du site.
     </div>
 </div>
 <?php include "footer.php"?>
