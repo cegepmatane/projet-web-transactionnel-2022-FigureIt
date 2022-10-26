6d5a6dc3588228cfefe61f56f2cb2b56f92a37c3
Moa <lucas.heidet@orange.fr>
2022-10-26 20:24:33 +0200

    #40 Ajout image et code html proto

---
+++ doc/Prototypes/Historique_achats.php
@@ -0,0 +1,54 @@
+<?php
+    $titre = "Historique";
+    $page = "historique";
+    include  "../header.php";
+?>
+<link rel="stylesheet" href="css/index.css">
+<div class="container">
+    <div class="content">
+        <div class="overflower">
+            <table class="liste">
+                <tr class="titres">
+                    <th>Id commande</th>
+                    <th>Date d'achat</th>
+                    <th>Quantité</th>
+                    <th>Prix</th>
+
+                </tr>
+
+                <tr>
+                    <td><a href="#">1254875</a></td>
+                    <td>JJ/MM/AAA</td>
+                    <td>3</td>
+                    <td>183.24€</td>
+                </tr>
+                <tr>
+                    <td><a href="#">1254875</a></td>
+                    <td>JJ/MM/AAA</td>
+                    <td>3</td>
+                    <td>183.24€</td>
+                </tr>
+                <tr>
+                    <td><a href="#">1254875</a></td>
+                    <td>JJ/MM/AAA</td>
+                    <td>3</td>
+                    <td>183.24€</td>
+                </tr>
+                <tr>
+                    <td><a href="#">1254875</a></td>
+                    <td>JJ/MM/AAA</td>
+                    <td>3</td>
+                    <td>183.24€</td>
+                </tr>
+                <tr>
+                    <td><a href="#">1254875</a></td>
+                    <td>JJ/MM/AAA</td>
+                    <td>3</td>
+                    <td>183.24€</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+
