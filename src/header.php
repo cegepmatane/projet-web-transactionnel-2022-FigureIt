22e468617e6bc30f8e2ed042064e31a7f6ccc30b
Moa <lucas.heidet@orange.fr>
2022-10-31 06:42:26 +0100

    #45 Détails

--- src/header.php
+++ src/header.php
@@ -4,7 +4,7 @@
 
 if ($_SESSION["lastConnexion"] < (time()-TEMPS_MAINTIENT) && $_SESSION["lastConnexion"] > (time() - UN_JOUR)){
     $_SESSION["loggedin"] = null;
-    header("location: ".SITE_URL."/connexion.php");
+    header("location: ".SITE_URL."connexion.php");
     exit;
 }else if($_SESSION["lastConnexion"] < (time() - UN_JOUR)){
     $_SESSION = array();
@@ -21,7 +21,7 @@ if ($_SESSION["lastConnexion"] < (time()-TEMPS_MAINTIENT) && $_SESSION["lastConn
           content="width=device-width, initial-scale=1">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="<?= SITEPATH ?> style.css" rel="stylesheet">
+    <link href="<?= SITE_URL ?>style.css" rel="stylesheet">
 </head>
 <body class="d-flex flex-column h-100">
 <div class="container">
@@ -34,7 +34,7 @@ if ($_SESSION["lastConnexion"] < (time()-TEMPS_MAINTIENT) && $_SESSION["lastConn
                 <?php
                     if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){ ?>
                         <a href="<?= SITE_URL."user/index.php"?>" class="user-profile"><?= $_SESSION["nom"]?></a>
-                        <a href="<?= SITE_URL."user/logout.php"?>">D&eacute;connexion</a>
+                        <a href="<?= SITE_URL."user/logout.php"?>" class="user-profile">D&eacute;connexion</a>
                 <?php }else{ ?>
                         <a href="<?= SITE_URL?>inscription.php" class="login">Inscription</a>
                         <a href="<?= SITE_URL?>connexion.php" class="login">Connexion</a>
