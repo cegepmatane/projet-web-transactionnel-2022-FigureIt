a3e05603813d1f8418ded224ee325026ccb5b7e3
Moa <lucas.heidet@orange.fr>
2022-12-07 22:55:42 +0100

    #49 pathlocale dans config.php

--- src/config.php
+++ src/config.php
@@ -1,13 +1,15 @@
 <?php
 
-if($_SERVER['HTTP_HOST'] == "localhost:8080"){// For local
- define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/projet-web-transactionnel-2022-FigureIt/src/');
- define('SITEPATH', $_SERVER['DOCUMENT_ROOT'] . '/projet-web-transactionnel-2022-FigureIt/src/');
+if($_SERVER['HTTP_HOST'] == "localhost"){// For local
+ define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/projet-web-transactionnel/projet-web-transactionnel/src/');
+ define('SITEPATH', $_SERVER['DOCUMENT_ROOT'] . '/projet-web-transactionnel/projet-web-transactionnel/src/');
 }
 else{ // For Web
  define('SITE_URL', "https://" . $_SERVER['HTTP_HOST'].'/');
  define('SITEPATH', $_SERVER['DOCUMENT_ROOT'].'/');
 }
+
+define('PATHLOCALE', $_SERVER['DOCUMENT_ROOT'].'/projet-web-transactionnel/projet-web-transactionnel/src/locales');
 
 const TEMPS_MAINTIENT = 1800;
 const UN_JOUR = 24*3600;
