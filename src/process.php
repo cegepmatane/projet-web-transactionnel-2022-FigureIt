11b2c52aff5ceaf8330ab76b678f6eb1930d42f0
BenjaminRodot <78915615+BenjaminRodot@users.noreply.github.com>
2022-12-07 14:44:31 -0500

    Traduction des erreurrs #48

--- src/process.php
+++ src/process.php
@@ -13,42 +13,42 @@ $password = $receivedDATA["password"];
 $passwordConfirm = $receivedDATA["confirmpassword"];
 error_log(print_r($receivedDATA, true));
 if (empty($receivedDATA['name'])) {
-    $errors['name'] = 'Name is required.';
-    
+    $errors['name'] = _('Name is required.');
+
 }else{
     // Check si l'identifiant est deja utilise
     if (!empty(ClientDAO::findClientByName($identifiant))){
-        $errors['name'] = "Identifiant d&eacute;j&agrave; utilis&eacute;";
+        $errors['name'] = _("Identifiant d&eacute;j&agrave; utilis&eacute;");
     }
 }
 
 if (empty($receivedDATA['email'])) {
-    $errors['email'] = 'Email is required.';
+    $errors['email'] = _('Email is required.');
     
 }else{
     // Check si l'email est deja utilise
     if (!empty(ClientDAO::findClientByEmail($email))){
-        $errors['email'] = "Adresse mail d&eacute;j&agrave; utilis&eacute;e";
+        $errors['email'] = _("Adresse mail d&eacute;j&agrave; utilis&eacute;e");
     }
 }
 
 if (empty($receivedDATA['password'])) {
-    $errors['password'] = 'Password is required.';
+    $errors['password'] = _('Password is required.');
 }
 
 if(empty($receivedDATA['confirmpassword'])){
-    $errors['confirmedPassword'] = 'Confirmed password is required.';
+    $errors['confirmedPassword'] = _('Confirmed password is required.');
 }
 if(!empty($receivedDATA['name'])&&!empty($receivedDATA['email'])&&!empty($receivedDATA['password'])&&!empty($receivedDATA['confirmpassword'])){
     if($receivedDATA['password'] != $receivedDATA['confirmpassword']){
-        $errors['confirmedPassword'] = 'Confirmed password is not the same as password.';
+        $errors['confirmedPassword'] = _('Confirmed password is not the same as password.');
     }
     
         //var_dump($err_email);
 
         // Check si il y a des espaces dans un des mots de passe
         if(preg_match("/\s/", $password) || preg_match("/\s/", $passwordConfirm)){
-            $errors['password'] = "Le mot de passe contient des espaces";
+            $errors['password'] = _("Le mot de passe contient des espaces");
         }
 
         if (empty($errors['name']) && empty($errors['email']) && empty($errors['password']) && empty($errors['confirmedPassword'])){
